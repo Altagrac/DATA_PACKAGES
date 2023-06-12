4bbefcfc84485f6c780a9cc55d7738d007f2578f
Esperance ASNGAR <esperance.asngar@gmail.com>
2023-06-12 22:24:51 +0200

    name changing in file functions

--- build/lib/codes_pkges/opersfunctions.py
+++ build/lib/codes_pkges/opersfunctions.py
@@ -3,21 +3,21 @@
 Operators functions for Data Analyst
 @author: asngar
 """
-# ******
+# ----
     ## -- Packages --
 
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import seaborn as sns
 
-    ## -- function which delete all variables with more than 90% of missing value --
-    
-def remov_mv90(df): 
+    # -- function which delete all variables with more than 90% of missing value --
+
+def remov_mv90(df):
     print("New data with only less than 90% of missing values ")
     df_new = df[df.columns[df.isna().sum()/df.shape[0] < 0.9]]
     return df_new
 
-    ## -- function which delete all ouliers values --
+    # -- function which delete all ouliers values --
 
 def outliers(df,target):
     Q1 = df["target"].quantile(0.25)
@@ -26,8 +26,62 @@ def outliers(df,target):
     No_outliers = df[~((df["target"] < (Q1-1.5*IQR)) | (df["target"] > (Q3 + 1.5*IQR)))]
     return No_outliers
 
-    ## -- function which split dataset according the target categori --
+    # -- function which split dataset according the target categori --
 def sub_target_df(df,target):
-    target_yes = df[df[target] == "positive"]  
+    target_yes = df[df[target] == "positive"]
     target_no = df[df[target] == "negative"]
     return   target_yes, target_n
+
+    # --- function which write dataset_seaborn list and read it afterward ---
+
+def names_dtaset_sns(filename):
+    dataset_names = sns.get_dataset_names()
+
+    with open(filename, 'w') as f:
+        for dataset_name in dataset_names:
+            f.write(dataset_name + '\n')
+
+    with open(filename, 'r') as f:
+        print(f.read())
+
+    # -- Missing values in a dataset --
+
+def check_missing_values(dataset):
+    """
+    Cette fonction prend en entrée un dataset (pandas dataframe) et vérifie si
+    le dataset contient des valeurs manquantes. Elle retourne le nombre de valeurs
+    manquantes pour chaque colonne.
+    """
+    missing_values_count = dataset.isnull().sum()
+    if missing_values_count.sum() == 0:
+        return f"Il n'y a pas de valeurs manquantes dans le dataset."
+    else:
+        print("Le dataset contient des valeurs manquantes :")
+        return missing_values_count
+
+    # -- Removing columns with missing values at 75%  --
+
+def rm_misvalues_cols(dataset):
+    """
+    Cette fonction prend en entrée un dataset (pandas dataframe) et supprime
+    les colonnes qui ont un nombre de valeurs manquantes supérieur au 3ème quartile.
+    """
+    # Calculer le 3ème quartile des valeurs manquantes pour chaque colonne
+    missing_values_count = dataset.isnull().sum()
+    quartile_3 = np.quantile(missing_values_count, 0.75)
+
+    # Sélectionner les colonnes à conserver (celles qui ont un nombre de valeurs manquantes
+    # inférieur ou égal au 3ème quartile)
+    columns_to_keep = missing_values_count[missing_values_count <= quartile_3].index
+
+    # Supprimer les colonnes à partir du dataset
+    dataset = dataset[columns_to_keep]
+
+    # Afficher un message pour indiquer les colonnes supprimées
+    removed_columns = set(missing_values_count.index) - set(columns_to_keep)
+    if len(removed_columns) > 0:
+        print("Les colonnes suivantes ont été supprimées en raison d'un grand nombre de valeurs manquantes :")
+        print(removed_columns)
+
+    # Retourner le dataset sans les colonnes supprimées
+    return dataset
--- codes_pkges/functions.py
+++ codes_pkges/functions.py
@@ -7,26 +7,26 @@ Created on Fri Feb  3 14:50:12 2023
 """
 # ******
     ## -- Packages --
-    
-import pandas as pd 
+
+import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
     ## -- function to print dataset shape: --
-    
-def dshape(df):
+
+def data_shape(df):
     print("This dataset contains:")
     # Nombers of rows :
     print(f"--> {df.shape[0]} lignes")
-    # Nombers of columns 
+    # Nombers of columns
     print(f"--> {df.shape[1]} colones")
-    # Data index 
+    # Data index
     print(f"--> {df.index}")
-       
+
 
     ## -- function which return values counts by data types ---
-    
+
 def data_types(df):
     list_num = []
     list_cat = []
@@ -38,20 +38,20 @@ def data_types(df):
     df1 = pd.DataFrame(list(list_num), columns=["Numerics_cols"])
     df2 = pd.DataFrame(list(list_cat), columns=["Categorials_cols"])
     df_typs = pd.concat([df1,df2],axis=1)
-    return df_typs, df.dtypes.value_counts().plot.pie() 
+    return df_typs, df.dtypes.value_counts().plot.pie()
 
     ## -- function which compute percentage of missing values --
-    
-def percentage_mv(df): 
+
+def prcentag_misval(df):
     print("Percentage of missing values by columns")
     df_perc = (df.isna().sum()/df.shape[0]).sort_values(ascending=True)
-    return df_perc 
+    return df_perc
 
-    ## -- function wich give some statistiques informations on numerics features -- 
-    
+    ## -- function wich give some statistiques informations on numerics features --
+
 def stats(df):
-    count=  df.describe().loc['count'] 
-    avrg  = df.describe().loc['mean'] 
+    count=  df.describe().loc['count']
+    avrg  = df.describe().loc['mean']
     Q1 = df.describe().loc['25%']
     median = df.describe().loc['50%']
     Q3 = df.describe().loc['75%']
@@ -59,10 +59,10 @@ def stats(df):
     return df_stat
 
     ## -- function which give more details on target : --
-    
+
 def info_target(df,target):
     print("Target values & percentage by categorie\n")
-    
+
     if df[target].dtypes == "int64":
         print(df[target].value_counts().to_frame())
     else :
@@ -85,41 +85,38 @@ def map_nan(df):
     sns.heatmap(df.isna(), cbar=False)
     plt.text(12, 12, textstr, fontsize=14)
     plt.show()
-    
-    ## -- fucntion to plot numerics features -- 
-    
+
+    ## -- fucntion to plot numerics features --
+
 def plot_num(df):
     for col in df.select_dtypes(exclude="object"):
         plt.figure()
         sns.histplot(df[col],kde=True)
         plt.title(f"Distribution of {col} Data")
         plt.tight_layout()
-      
+
      ## -- fucntion to plot categorics features --
-        
+
 def plot_cat(df):
     green_diamond = dict(markerfacecolor='g', marker='D')
     for col in df.select_dtypes(include="object"):
-       # print(f"{col :-<30} {df[col].unique()}") : print with clean style 
+       # print(f"{col :-<30} {df[col].unique()}") : print with clean style
         plt.figure()
         df[col].value_counts().plot.box(flierprops=green_diamond)
         plt.title(f"Distribution of {col} Data")
         #plt.tight_layout()
 
     ## -- function to plot all dataset --
-    
-def ditrib_df(data):
-   #Histrogramme de distribution par variable 
+
+def df_distribution(data):
+   #Histrogramme de distribution par variable
     plt.figure(figsize=(15,10))
     for i,col in enumerate(data.columns,1):
         plt.subplot(4,3,i)
         plt.title(f"Distribution of {col} Data")
         sns.histplot(data[col],kde=True)
         plt.tight_layout()
-        
+
     filename = "Ensemble de distributions par colones"
     plt.savefig(filename+'.png')
     plt.show();
-    
-
-
